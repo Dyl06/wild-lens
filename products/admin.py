0f8a30095214089a6a12cd42ea4c6bdf7a926351
Dyl06 <dylhndrsn@gmail.com>
2023-08-10 12:38:50 +0000

    Customized admin and added automatic sku generation for new products

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Products, Category
-
+from .models import Product, Category, SubCategory
 # Register your models here.
-admin.site.register(Products)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'photographer',
+        'price',
+        'image',
+    )
+
+    exclude = ('sku',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.sku:
+            obj.sku = Product.generate_sku()
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
+admin.site.register(SubCategory)
--- products/models.py
+++ products/models.py
@@ -1,23 +1,57 @@
-from django.db import models
+from django.db import models, transaction
 from photographer.models import Photographer
-
+import uuid
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
 
     def __str__(self):
         return self.name
 
 
-class Products(models.Model):
-    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+class SubCategory(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'SubCategories'
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ManyToManyField('Category')
+    subcategory = models.ManyToManyField('SubCategory')
     photographer = models.ForeignKey(Photographer, on_delete=models.CASCADE)
-    sku = models.CharField(max_length=50, unique=True)
+    sku = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024)
     image = models.ImageField()
+
+    @classmethod
+    def generate_sku(cls):
+        with transaction.atomic():
+            last_sku_instance = cls.objects.select_for_update().order_by('-id').first()
+
+            if last_sku_instance:
+                last_sku = last_sku_instance.sku
+                counter = int(last_sku[6:]) + 1
+                new_sku = f'PROD{counter:06}'
+            else:
+                new_sku = 'PROD000001'
+
+            return new_sku
+
+    def save(self, *args, **kwargs):
+        if not self.sku:
+            self.sku = self.generate_sku()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
