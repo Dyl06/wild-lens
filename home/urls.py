54cb5a20391e446e55b2c863f7a584eb737a047d
Dyl06 <dylhndrsn@gmail.com>
2023-08-21 10:15:19 +0000

    Added login functionality and some styling to login and register pages

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path("register", views.register_request, name="register"),
-    path("login", views.register_request, name="login"),
+    path("login", views.login_request, name="login"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
-
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 
@@ -23,3 +23,34 @@ def register_request(request):
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request=request, template_name="home/register.html", context={"register_form": form})
+
+def login_request(request):
+    if request.method == "POST":
+        email = request.POST['email'],
+        password = request.POST['password'],
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user),
+            messages.success(request, f'Welcome back {{user.username}}'),
+            return redirect('home/index.html'),
+        else:
+            messages.error(request, 'Invalid email or password, please try again.')
+
+    return render(request, 'home/login.html')
+
+    
+	# 	form = AuthenticationForm(request, data=request.POST)
+	# 	if form.is_valid():
+	# 		username = form.cleaned_data.get('username')
+	# 		password = form.cleaned_data.get('password')
+	# 		user = authenticate(username=username, password=password)
+	# 		if user is not None:
+	# 			login(request, user)
+	# 			messages.info(request, f"You are now logged in as {username}.")
+	# 			return redirect("home/index.html")
+	# 		else:
+	# 			messages.error(request, "Invalid username or password.")
+	# 	else:
+	# 		messages.error(request, "Invalid username or password.")
+	# form = AuthenticationForm()
+	# return render(request=request, template_name="home/login.html", context={"login_form":form})
