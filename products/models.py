992ecb76b2b4b65dfc0aab7cdfedf4b0deb1208f
Dyl06 <dylhndrsn@gmail.com>
2023-08-22 12:01:47 +0000

    Added sizes to product model and product page and checkout template

--- products/models.py
+++ products/models.py
@@ -36,6 +36,12 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField()
+    SIZE_CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    ]
+    size = models.CharField(max_length=1, choices=SIZE_CHOICES, default='S')
 
     @classmethod
     def generate_sku(cls):
