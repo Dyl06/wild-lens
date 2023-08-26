9eec8a0b6a6df0e826f66079b6404592947a6f89
Dyl06 <dylhndrsn@gmail.com>
2023-08-26 20:53:44 +0000

    Added checkout success logic

--- bag/views.py
+++ bag/views.py
@@ -144,8 +144,8 @@ def adjust_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
+    if 'size' in request.POST:
+        size = request.POST['size']
     bag = request.session.get('bag', {})
 
     if size:
