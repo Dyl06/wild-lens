053e15873fbea18ac908c10b222baf2a8a0a4ff3
Dyl06 <dylhndrsn@gmail.com>
2023-08-29 10:41:43 +0000

    Removed outdated edit profile view, url and template

--- photographer/urls.py
+++ photographer/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('photographer/<int:photographer_id>/', views.photographer_profile,
          name='photographer-profile'),
-    path('edit-profile/', views.edit_profile, name='edit-profile'),
     path('', views.all_photographers, name='photographers'),
     path('<int:photographer_id>/', views.photographer_page,
          name='photographer_page'),
--- photographer/views.py
+++ photographer/views.py
@@ -20,49 +20,6 @@ def photographer_profile(request):
     return render(request, 'photographer/photographer-profile.html')
 
 
-@login_required
-def edit_profile(request):
-
-    if not self.is_photographer(request):
-        # error
-        return
-
-    user = request.user
-    try:
-        profile = Photographer.objects.get(user=user)
-    except Photographer.DoesNotExist:
-        # Create a profile if it doesn't exist
-        # profile = Photographer(user=user)
-        # profile.save()
-        return redirect('/')
-
-    if request.method == 'POST':
-        # Update the profile fields with the submitted data
-        profile.name = request.POST['name']
-        profile.bio = request.POST['bio']
-        profile.location = request.POST['location']
-        profile.website = request.POST['website']
-        profile.social_facebook = request.POST['social_facebook']
-        profile.social_instagram = request.POST['social_instagram']
-        profile.phone = request.POST['phone']
-
-        # Check if a new profile picture was uploaded
-        if 'profile_picture' in request.FILES:
-            profile.profile_picture = request.FILES['profile_picture']
-
-        # Save the changes
-        profile.save()
-        user.email = request.POST['email']
-        user.save()
-
-        return redirect('photographer-profile')
-
-    return render(request, 'photographer/edit-profile.html', {'user': user})
-
-
-# Create your views here.
-
-
 def all_photographers(request):
     """ A view to show all the photographers on the site """
 
